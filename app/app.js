041ef386a78effe6546cfa4db0a593d6f5ead100
TommyCakes <tomhwarhurst@aol.com>
2015-12-30 20:26:37 +0000

    Created routes for page

--- app/app.js
+++ app/app.js
@@ -1,1 +1,27 @@
 angular.module('App', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+  .state('home', {
+    url: "/",
+    templateUrl: 'app/states/home.html'
+  })
+  .state('store', {
+    url: "/store",
+    templateUrl: 'app/states/store.html'
+  })
+  .state('about', {
+    url: "/about",
+    templateUrl: 'app/states/about.html'
+  })
+  .state('contact', {
+    url: "/contact",
+    templateUrl: 'app/states/contact.html'
+  })
+  .state('cart', {
+    url: "/cart",
+    templateUrl: 'app/states/cart.html'
+  })
+})
